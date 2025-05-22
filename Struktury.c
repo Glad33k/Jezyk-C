591063c234055b767284d398bd8fcbef0be0c17f
Glad33k <Dawid.gladek@gmail.com>
2025-05-22 12:56:55 +0200

    Update Struktury.c

--- Struktury.c
+++ Struktury.c
@@ -96,6 +96,88 @@ struct zespolone dodaj(struct zespolone liczba1, struct zespolone liczba2)
 ///iloczyn, iloraz na zespolonych
 
 
+struct zespolone iloczyn(struct zespolone liczba1, struct zespolone liczba2)
+{
+    struct zespolone suma;
+    suma.a=liczba1.a*liczba2.a-liczba1.b*liczba2.b;
+    suma.b=liczba1.a*liczba2.b+liczba1.b*liczba2.a;
+    return suma;
+};
+
+union super_int{
+int i;
+unsigned int u;
+};
+
+
+
+struct element{
+int i;
+struct element *next;
+};
+
+
+struct element *utworz()
+{
+    return NULL;
+};
+
+
+void wyczysc(struct element* lista)
+{
+    struct element *wsk=lista;
+    while(lista!=NULL)
+    {
+        lista=lista->next;
+        free(wsk);
+        wsk=lista;
+    }
+}
+
+
+struct element *dodajnapoczatek(struct element*lista, int a)
+{
+    struct element *wsk=malloc(sizeof(struct element));
+    wsk->i=a;
+    wsk->next=lista;
+    return wsk;
+};
+
+
+struct element *dodajnakoniec(struct element *lista, int a)
+{
+    struct element *wsk;
+    if(lista==NULL)
+    {
+        lista=wsk=malloc(sizeof(struct element));
+    }
+    else
+    {
+        wsk=lista;
+        while(wsk->next!=NULL);
+        {
+            wsk=wsk->next;
+        }
+        wsk->next=malloc(sizeof(struct element));
+        wsk=wsk->next;
+    }
+    wsk->i=a;
+    wsk->next=NULL;
+    return lista;
+
+};
+
+
+void wypiszlista(struct element *lista)
+{
+    while(lista!=NULL)
+    {
+        printf("%d \n",lista->i);
+        lista=lista->next;
+    }
+    printf("\n");
+}
+
 
 int main()
 {
@@ -146,7 +228,59 @@ int main()
 
     ///7.2.7
     printf("\n\n CW7_2_7\n\n");
-    
+    //struct zespolone Y;
+    ///Y.a=4;
+    //Y.b=9;
+    //wyswietl_liczbe_zespolona(Y);
+    //struct zespolone X;
+    //wczytaj(X);
+    //wyswietl_liczbe_zespolona(X);
 
+
+    ///DODATKOWE
+    printf("\n\n CWDODATKOWE\n\n");
+   //struct zespolone X;
+   //X.a=5;
+   // X.b=2;
+    //struct zespolone wynik;
+    //wynik=iloczyn(X,Y);
+    //wyswietl_liczbe_zespolona(wynik);
+
+    ///7.2.10
+    printf("\n\n CW7_2_10\n\n");
+    //struct wiele_int X;
+    //union super_int Y;'
+    //printf("%d\n",sizeof(int));
+    //printf("%d\n",sizeof(unsigned int));
+    ///struktura
+    //printf("%d\n",sizeof(X));
+    //printf("%p\n",X);
+    //printf("%p\n",X.i);
+    //printf("%p\n",X.u);
+    ///Unia
+    //printf("%d\n",sizeof(Y));
+    //printf("%p\n",Y);
+    //printf("%p\n",Y.i);
+    //printf("%p\n",Y.u);
+
+
+    ///7.3.1 i 2
+    printf("\n\n CW7_3_1\n\n");
+
+    ///lista bez glowy
+    ///Cw nie mam pojecia ktore juz bo sie zgubilem
+    struct element *lista=utworz();
+    lista=dodajnakoniec(lista,4);
+    wypiszlista(lista);
+    lista=dodajnakoniec(lista,15);
+    wypiszlista(lista);
+    lista=dodajnakoniec(lista,3);
+    wypiszlista(lista);
+    lista=dodajnakoniec(lista,8);
+    wypiszlista(lista);
+    lista=dodajnakoniec(lista,10);
+    wypiszlista(lista);
+    lista=dodajnapoczatek(lista,6);
+    wypiszlista(lista);
     return 0;
 }
